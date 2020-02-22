2e52b09c074f585b6d1a581478388c8f4a64ea2a
Ethan-Reeds <ethancreeds@gmail.com>
2020-02-22 14:32:23 -0500

    Ethan Reeds and Makarand Lahane - COOKIES

--- JavaApplication20/test/MainNGTest.java
+++ JavaApplication20/test/MainNGTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 
 /**
  *
@@ -20,6 +22,19 @@ public class MainNGTest {
     public MainNGTest() {
     }
 
+    static CookieManager cookieManager = new CookieManager();
+
+    @BeforeClass
+    public static void setupcookie() {
+        CookieHandler.setDefault(cookieManager);
+
+    }
+
+    @BeforeMethod
+    public void clearCookie() {
+        cookieManager.getCookieStore().removeAll();
+    }
+    
     @BeforeClass
     public static void setUpClass() throws Exception {
     }
